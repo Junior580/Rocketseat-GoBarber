03b3c11892655717ba52abefb4b07a40427015d8
junior <junior.msm25@gmail.com>
2026-09-06 10:39:34 -0300

    feat(notifications): add appointment notifications

---
+++ server/src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,30 @@
+import { AppDataSourceMongo } from '@shared/infra/typeorm/mongoData-source'
+import { MongoRepository } from 'typeorm'
+
+import { INotificationsRepository } from '@modules/notifications/repositories/interface/INotificationsRepository'
+import { ICreateNotificationDTO } from '@modules/notifications/dtos/ICreateNotificationsDTO'
+
+import { Notification } from '../schemas/Notification'
+
+export class NotificationsRepository implements INotificationsRepository {
+  private notificationRepository: MongoRepository<Notification>
+
+  constructor() {
+    this.notificationRepository =
+      AppDataSourceMongo.getMongoRepository(Notification)
+  }
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.notificationRepository.create({
+      content,
+      recipient_id,
+    })
+
+    await this.notificationRepository.save(notification)
+
+    return notification
+  }
+}
