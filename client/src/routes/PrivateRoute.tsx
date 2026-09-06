4bbbc31a6920682348e4bebefb0a3e791b6eb44a
junior <junior.msm25@gmail.com>
2026-09-06 10:39:35 -0300

    feat(routes): add private routes and app entry

---
+++ client/src/routes/PrivateRoute.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from '../hooks/auth'
+
+type RouteProps = {
+  children?: ReactNode
+  isPrivate?: boolean
+}
+
+export const PrivateRoute: React.FC<RouteProps> = ({
+  children,
+  isPrivate = false,
+}) => {
+  const { user } = useAuth()
+
+  // private NO user YES = false == navigate to /dashboard
+  // private YES user NO = false == navigate to home
+
+  // private YES user YES = true == children || outlet
+  // private NO user NO = true == children || outlet
+
+  return isPrivate === !!user ? (
+    <>
+      {children} || <Outlet />
+    </>
+  ) : (
+    <Navigate to={user ? '/dashboard' : '/'} />
+  )
+}
